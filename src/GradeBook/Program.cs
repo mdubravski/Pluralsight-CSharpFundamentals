9d738f83d280413ddce229e1a9763d2916aa37b7
mdubravski <mdubravski@gmail.com>
2022-06-23 09:08:35 -0400

    add gradebook examples

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            float x = 34.1f; 
+            float y = 69.5f;
+            Console.WriteLine(x+y);
+
+            if(args.Length > 0) 
+                Console.WriteLine($"Hello {args[0]}!");
+            else
+                Console.WriteLine("Hello!");
+        }
+    }
+}
